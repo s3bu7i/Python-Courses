38e6100cf18b2ad6718dbf6f9524b8cd05c310a0
Sabuhi Gasimzada <sabuhi.qasimzada2021@khazar.org>
2023-09-22 17:05:39 +0400

    *

---
+++ Dərs 18/project/main/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    
+    
+    path('',views.index,name='index'),
+    path('haqqinda/',views.about,name='about'),
+    path('elaqe/',views.elaqe,name='elaqe'),
+    path('calculator/',views.calculator,name='calculator'),
+    path('login/',views.login,name='login'),
+    path('radio/',views.radio,name='radio'),
+    path('exam/',views.exam,name='exam'),
+    path('select/',views.select,name='select'),
+    path('select_exchange/',views.select_exchange,name='select_exchange'),
+    path('army/',views.step,name='step'),
+    
+    
+    
+    path('crud/',views.crud,name='crud'),
+    
+    path('delete/<int:id>',views.delete,name='delete'),
+    
+    path('edit/<int:id>',views.edit,name='edit')
+ 
+
+
+ 
+
+    
+]
