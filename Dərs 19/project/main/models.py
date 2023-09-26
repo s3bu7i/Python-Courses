a09395cacdb59cf9729ba90d4600e5eaf18a5990
Sabuhi Gasimzada <sabuhi.qasimzada2021@khazar.org>
2023-09-26 19:11:32 +0400

    *

---
+++ Dərs 19/project/main/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Student(models.Model):
+    
+    ad = models.CharField(max_length=50,null=True,blank=True)
+    metn = models.TextField(max_length=700,null=True,blank=True)
+    tarix = models.DateField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.ad
+    
+class Telefon(models.Model):
+    
+    model = models.CharField(max_length=100,null=True,blank=True)
+    dioqonal = models.CharField(max_length=100,null=True,blank=True)
+    prosessor = models.CharField(max_length=100,null=True,blank=True)
+    nuve_sayi = models.CharField(max_length=100,null=True,blank=True)
+    ram = models.CharField(max_length=100,null=True,blank=True)
+    
+    
+    
+    def __str__(self):
+        return self.model
+    
+    
+    
