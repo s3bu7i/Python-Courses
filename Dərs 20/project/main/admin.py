8b21cd2b26345aeb5a5ee32757197491fe368bd2
Sabuhi Gasimzada <sabuhi.qasimzada2021@khazar.org>
2023-09-28 21:15:38 +0400

    *

---
+++ Dərs 20/project/main/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+
+admin.site.register(Student)
+admin.site.register(Telefon)
+admin.site.register(Shop)
+
+
+admin.site.site_header = 'Django admine xosh geldiniz'
+admin.site.site_title
+admin.site.site_url
